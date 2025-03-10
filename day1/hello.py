efcae587d7c77951b461cc51447889052e1cb0a8
akhil-06 <svam1008@gmail.com>
2025-03-10 22:33:59 +0530

    day-1 intro

--- day1/hello.py
+++ day1/hello.py
@@ -1,3 +1,7 @@
+# classroom link:- https://classroom.google.com/c/NzU4MTUxOTYzOTYw?cjc=eqjohnq
+# Classroom Codde:- eqjohnq
+
+
 print("Hello World!!!");
 # print("Hello World!!!");   # This is a comment
 print("Hello World!!!");
