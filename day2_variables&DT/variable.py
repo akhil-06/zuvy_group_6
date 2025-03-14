dfa504f85cb941dbf9e162dc837987f80947fc0c
akhil-06 <svam1008@gmail.com>
2025-03-14 21:47:25 +0530

    day 2

---
+++ day2_variables&DT/variable.py
@@ -0,0 +1,166 @@
+# Conditionals
+# 2.if - else statement
+age = 10;
+if age>=18:
+    print("You are eligible to vote");
+else:
+    print("You are not eligible to vote");
+
+
+# 1. if 
+# age = 20;
+# if age>=18:
+#     print("You are eligible to vote");
+    
+# # short hand
+# if age>=18: print("You are eligible to vote");
+    
+
+
+
+# User Input In Python
+# a = int(input("Enter a number: "));
+# print(a);
+# print(type(a));
+
+
+
+# Type Conversion
+# Explicit Type Conversion
+# int to string
+# a = "72634826";
+# b = int(a);
+# print(b);
+# print(type(b));
+
+
+# float to string
+# a = 5.8457;
+# b = str(a);
+# print(b);
+# print(type(b));
+
+
+
+# String to float
+# a = "5.90";
+# b = float(a);
+# print(b);
+# print(type(b));
+
+
+
+
+# float to int
+# a = 5.64;
+# b = int(a);
+# print(b);
+# print(type(b));
+
+
+
+# int to float
+# a = 5;
+# b = float(a);
+# print(b);
+# print(type(b));
+
+
+# Implicit Type Conversion
+# a = 4;
+# b = 4.25;
+# print(a+b);
+# print(type(a+b));
+
+
+
+
+
+
+# operators
+# Logical Operators
+# a = 10
+# b = 20
+# # print(not (a>b))
+# print(a>b and a<b)
+# print(a>b or a<b)
+
+
+
+
+
+# Assignment Operators
+# a = 10;
+# b = 20;
+# b-=5;
+# print(b);
+# b+=5;
+# print(b);
+# b*=5;
+# print(b);
+# b/=5;
+# print(b);
+# b%=5;
+# print(b);
+# b**=5;
+# print(b);
+# b//=5;
+# print(b);
+
+
+
+
+# Relational Operators
+# a = 10;
+# b = 20;
+# print(a>b);
+# print(a<b);
+# print(a>=b);
+# print(a<=b);
+# print(a==b);
+# print(a!=b);
+
+
+# Arithematic Operators
+# a = 10;
+# b = 20;
+# print("Addition: ", a + b);
+# print("Substraction: ", a - b);
+# print("Multiplication: ", a * b);
+# print("Division: ", a / b);
+# print("Modulus: ", a % b);
+# print("Exponential: ", a ** b);
+# print("Floor Division: ", a // b);
+
+
+
+
+# Data Types in Python
+# age = 22;
+# name = "Akhil Sharma";
+# is_student = True;
+# marks = 90.5000;
+# marraige = None;
+# print("Name: ", type(name));
+# print("Age: ", type(age));
+# print("Is Student: ", type(is_student));
+# print("Marks: ", type(marks));
+# print("Marraige: ", type(marraige));
+
+# print("Name: ", name);
+# print("Age: ", age);
+# print("Is Student: ", is_student);
+# print("Marks: ", marks);
+# print("Marraige: ", marraige);
+
+
+
+
+
+
+# myVariablekjẓdvkzdbkfvgbzkjdsgvbjsdbvjksdbvkdbvksbdvkdsbvkbdvmsbdvkSbdvkzsbdvkzbdvmzsdbvkszdbvmzdbv = "Akhil Sharma";
+# age = 22;
+# marks = 90.5;
+# print("Name: ", myVariablekjẓdvkzdbkfvgbzkjdsgvbjsdbvjksdbvkdbvksbdvkdsbvkbdvmsbdvkSbdvkzsbdvkzbdvmzsdbvkszdbvmzdbv);
+# print("Age: ", age);
+# print("Marks: ", marks);
