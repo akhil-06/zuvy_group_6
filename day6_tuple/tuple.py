e901c660c91f93d5b55494bba37f5d326e061c46
akhil-06 <svam1008@gmail.com>
2025-03-22 23:05:46 +0530

    tuple and leetcode questions

---
+++ day6_tuple/tuple.py
@@ -0,0 +1,108 @@
+# tuple
+# ways to create a tuple in python
+# t = (10,20,30,40,50,60,70,80,90,100);
+t2 = 10,20,30;
+# print(t2);
+# t3 = tuple([10,20,30,40,50,60]);
+# print(t3);
+# t4 = (10,)
+# print(t4);
+# tuple packing
+a,b,c = 10,20,30;
+t5 = (a,b,c);
+print(t5);
+
+
+
+
+
+# list to tuple
+# lst = [10,20,30,40,50,60,70,80,90,100];
+# t = tuple(lst);
+# print(t);
+
+# t1 = (10,20,30);
+# t2 = t1 + 40;
+# print(t2);
+
+
+# t = (10,20,30,40,50,60,70,80,90,100, False, "Akhil",10.5);
+# print(len(t));
+
+
+
+# delete in tuple
+# t = (10,20,30,40,50,60,70,80,90,100);
+# # del t[0];
+# del t;
+# print(t);
+
+
+
+
+
+# slicing in tuple
+# t = (10,20,30,40,50,60,70,80,90,100);
+# print(t[-5:-10]);
+# print(t[-8:-5]);
+# print(t[::-1]);
+# print(t[8:11])
+# print(t[0:5]);
+# print(t[5:8]);
+# print(t)
+
+
+
+
+# repetation of tuples
+# t = (10,)*10;
+# print(t);
+
+
+
+
+# nesting of tuples
+# t1 = (10,20,30);
+# t2 = ('python', 'javascript')
+# t3 = (t1, (t2));
+# print(t3);
+
+
+
+
+# concatenation of two tuples
+# t1 = (10,20,30);
+# t2 = (40,50,60);
+# t3 = t1 + t2;
+# print(t3);
+# print(t1)
+# print(t2)
+
+
+
+# t = (10,20,30, 40, 50, 60, 70, 80, 90, 100);
+# for i in t:
+#     print(i);
+# print(t);
+
+
+
+
+
+
+
+
+# t = (10,20,30);
+# print(t[2]);
+# print(t[0])
+# print(t[1])
+
+
+# t = (10,20,30);
+# t1 = [10,20,30];
+# # t[0] = 100;
+# t1[0] = 100; 
+# print(t);
+# print(t1);
+# # print(type(t));
+# # print(type(t1));
